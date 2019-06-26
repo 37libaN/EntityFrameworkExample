5962d10df5596a6a8e9b1e9692a4b0f20b99c3f9
Nabil <20hussan@elmbrookstudents.org>
2019-06-26 10:25:49 -0500

    added the rest

---
+++ EntityFrameworkExample/Controllers/BarrelsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EntityFrameworkExample.Data.Context;
+using EntityFrameworkExample.Models;
+using EntityFrameworkExample.Service;
+
+namespace EntityFrameworkExample.Controllers
+{
+    public class BarrelsController : Controller
+    {
+        private BarrelService service;
+        public BarrelsController()
+        {
+            service = new BarrelService();
+        }
+    }
+}
